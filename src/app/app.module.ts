09fb78b47eac2d09f1753685c5bbaaf6e386ee52
CarolLimav <carol.lima.vitoria@gmail.com>
2025-03-02 23:09:27 -0300

    add introduction

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,20 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { IntroducaoComponent } from './introducao/introducao.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    IntroducaoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/introducao/introducao.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { AnimationItem } from 'lottie-web';
+
+
+@Component({
+  selector: 'app-introducao',
+  templateUrl: './introducao.component.html',
+  styleUrl: './introducao.component.scss'
+})
+export class IntroducaoComponent {
+  
+}
